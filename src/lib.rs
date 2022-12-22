d91da4cfa96f3f448699c254b4c93a5aa303a7e1
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2022-12-21 22:56:18 -0800

    Remove boilerplate function added by 'maturin init'

--- src/lib.rs
+++ src/lib.rs
@@ -31,16 +31,9 @@ impl Drop for Listener {
 	}
 }
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn terminal_input(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_class::<Listener>()?;
     Ok(())
 }
